4218f3f5fd38fa9583e0665d90345f617b3e29e4
Diego Cabulo <gcabulo@gmail.com>
2020-09-05 17:24:00 -0500

    Finale stage

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,12 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import Todos from "./Todos";
+import Addtodo from "./AddTodo";
 
-function App() {
-  return (
-    <div className="App">
-      <h1> My first react app</h1>
-      <p>Welcome </p>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: "buy some milk" },
+      { id: 2, content: "play mario kart" },
+    ],
+  };
+  deleteTodo = (id) => {
+    const todos = this.state.todos.filter((todo) => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos,
+    });
+  };
+
+  addTodo = (todo) => {
+    todo.id = this.state.todos.length + 1;
+    let todos = [...this.state.todos, todo];
+    this.setState({
+      todos
+    });
+  };
+  render() {
+    return (
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={this.state.todos} deleteTodo={this.deleteTodo} />
+        <Addtodo addTodo={this.addTodo} />
+      </div>
+    );
+  }
 }
 
 export default App;
